df7c133e73bfb3297afd9f457dd81580c86ce7cc
AnatolySoroka <toliksoroka66@gmail.com>
2023-01-15 10:14:41 +0200

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const itemsList = ingredients.map(item => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = item;
+  return itemEl;
+});
+
+ingredientsEl.append(...itemsList);
