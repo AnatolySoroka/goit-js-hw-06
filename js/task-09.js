653e7e56899e928de195b8bd9bc5509e7f83d04b
AnatolySoroka <toliksoroka66@gmail.com>
2023-01-22 15:19:12 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)
+    }`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  btn: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+}
+
+refs.btn.addEventListener('click', () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.color.textContent = getRandomHexColor();
+})
