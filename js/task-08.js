ea24f80500c03d8e5781d722447b16eb8b5d19a4
AnatolySoroka <toliksoroka66@gmail.com>
2023-01-22 15:05:58 +0200

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const data = {};
+
+    const formData = new FormData(event.currentTarget);
+
+    formData.forEach((value, name) => {
+        if (value === '') {
+            alert(`Поле ${name} повинне бути заповнене!`);
+        }
+        else {
+            data[name] = value;
+        }
+    });
+
+    this.reset();
+
+    console.log(data);
+}
